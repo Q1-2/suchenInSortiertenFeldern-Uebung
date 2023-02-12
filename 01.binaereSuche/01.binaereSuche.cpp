2dfd21c1c4ba41d11c27de15378cf212854fd3dc
aschwaiger74 <78038701+aschwaiger74@users.noreply.github.com>
2023-02-12 11:36:40 +0100

    Tipps hinzugefuegt

--- 01.binaereSuche/01.binaereSuche.cpp
+++ 01.binaereSuche/01.binaereSuche.cpp
@@ -9,11 +9,13 @@ int main()
 	int zahlen[] = { -2, 0, 3, 4, 5, 10 };
 	int elemente = sizeof(zahlen) / sizeof(int);
 
-	for (int i : zahlen)  cout << i << " ";
+	for (int i : zahlen) {
+		cout << i << " ";
+	}
 
 	// Tests: 
-	for (int gesuchteZahl = -2; gesuchteZahl <= 10; gesuchteZahl+=2) {
-		cout << "gesucht: " << gesuchteZahl <<" - entahlten: " << istEnthalten(zahlen, elemente, gesuchteZahl) << endl;
+	for (int gesuchteZahl = -2; gesuchteZahl <= 10; gesuchteZahl += 2) {
+		cout << "gesucht: " << gesuchteZahl << " - entahlten: " << istEnthalten(zahlen, elemente, gesuchteZahl) << endl;
 	}
 	return 0;
 }
@@ -21,7 +23,15 @@ int main()
 
 bool istEnthalten(int* zahlen, int elemente, int z)
 {
-	
+	// int elemente = sizeof a / sizeof(int); nicht möglich da nur Zeiger; erstes Zeichen!
+
+	// Wiederhole solange ...
+		// Ist der Wert in der Mitte zu groß? Dann suche rechts davon weiter.
+			// die Mitte ist der neue Index für "rechts" + 1
+		// Ist der Wert in der Mitte zu klein? Dann suche links davon weiter.
+			// die Mitte ist der neue Index für "links" - 1 
+	// Keines von beiden? Zahl gefunden! 
+
 
 	return false;
 }
